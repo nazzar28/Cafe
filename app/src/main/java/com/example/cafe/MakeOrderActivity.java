e5ca6004b778baca78d76735910ba6234eac58d4
nazzar28 <talantbekovnazar28@gmail.com>
2024-07-14 16:30:32 +0600

    second site design

--- app/src/main/java/com/example/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderActivity.java
@@ -1,6 +1,5 @@
 package com.example.cafe;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,20 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MakeOrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_make_order);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Intent intent = new Intent(this, MakeOrderActivity.class);
-        startActivity(intent);
     }
 }
