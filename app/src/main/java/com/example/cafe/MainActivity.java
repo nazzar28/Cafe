c66989faa324bcebe88831a4d6ac26dc3624631b
nazzar28 <talantbekovnazar28@gmail.com>
2024-07-15 13:03:44 +0600

    logic of first site

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.cafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +27,33 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Intent intent = new Intent(this, MakeOrderActivity.class);
+        EditText name = findViewById(R.id.editTextName);
+
+        EditText password = findViewById(R.id.editTextPassword);
+
+        Button signIn = findViewById(R.id.signInButton);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = name.getText().toString().trim();
+                String userPassword = password.getText().toString().trim();
+
+                if(username.isEmpty() || userPassword.isEmpty()){
+                    Toast.makeText(
+                            MainActivity.this,
+                            R.string.please_fill_all_fields,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }else{
+                    launchNextScreen(username);
+                }
+            }
+        });
+    }
+
+    private void launchNextScreen(String username){
+        Intent intent = MakeOrderActivity.newIntent(this, username);
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cafe/OrderDetailActivity.java
+++ app/src/main/java/com/example/cafe/OrderDetailActivity.java
@@ -1,7 +1,5 @@
 package com.example.cafe;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MakeOrderActivity extends AppCompatActivity {
+public class OrderDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_make_order);
+        setContentView(R.layout.activity_order_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public static Intent newIntent(Context context, String username){
-        Intent intent = new Intent(context, MakeOrderActivity.class);
-
-        intent.putExtra("username", username);
-
-        return intent;
     }
 }
